64d797ce8b517a9450a83b66df1553e643ba2673
Mark R <mirubenstein@gmail.com>
2018-01-01 16:25:44 -0800

    Update to_enum to enum_for to give more info about the method returning enumerator

--- strain.rb
+++ strain.rb
@@ -1,12 +1,13 @@
 module Strain
   def discard
-    return self.to_enum { size } unless block_given?
+    return enum_for(__method__) { size } unless block_given?
     each_with_object([]) { |value, output| output << value unless yield(value) }
   end
 
   def keep
-    return self.to_enum { size } unless block_given?
+    return enum_for(__method__) { size } unless block_given?
     each_with_object([]) { |value, output| output << value if yield(value) }
   end
 end
+
 Array.include Strain
